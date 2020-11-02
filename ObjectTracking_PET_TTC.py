4a8427bb3a35201e8f7c021ab94124d68dfde66b
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2020-11-02 11:08:31 -0500

    Update ObjectTracking_PET_TTC.py
    Use determinant to find parallel case

--- ObjectTracking_PET_TTC.py
+++ ObjectTracking_PET_TTC.py
@@ -5,13 +5,11 @@ def metric(position1, position2, velocity1, velocity2):
     """
     (cx1, cy1), (cx2, cy2) = position1, position2
     (vx1, vy1), (vx2, vy2) = velocity1, velocity2
-    # Both are non-moving
+
+    # Both Non-moving Case
     if set(velocity1+velocity2) == {0}:
         return None, None
-    # Both are at the same point
-    if all([x == y for x, y in zip(position1, position2)]):
-        return 0, 0
-    # One is not moving
+    # One Stationary Case
     if set(velocity1) == {0} or set(velocity2) == {0}:
         if set(velocity1) == {0}:  # See which one is moving
             dx_dy = [x - y for x,y in zip(position1, position2)]
@@ -33,7 +31,7 @@ def metric(position1, position2, velocity1, velocity2):
             for x, y in zip(dx_dy, velocity2):
                 if x == 0 and y == 0:
                     continue
-                elif (x == 0) ^ (y == 0):  # Movement is not needed and present or is needed and not present
+                elif (x == 0) ^ (y == 0):  # Movement is not needed and is present or is needed and not present
                     return None, None
                 else:
                     tx_ty.append(x/y)  # Time to reach x/y coordinate of intersecting point
@@ -41,18 +39,15 @@ def metric(position1, position2, velocity1, velocity2):
                 return tx_ty[0], tx_ty[0]
             else:  # Not intersecting
                 return None, None
-    D = []
-    # Linear Independence Test
-    for x, y in zip(velocity1, velocity2):  # Find linear dependence (parallel movement)
-        if (x==0 or y==0) and x != y:  # Intersecting lines (No dependence)
-            break
-        elif x == 0 and y == 0:  # Both are zero (might be moving parallel or same track)
-            #D.append(None)
-            continue  # Linear dependence in 2D
-        else:
-            D.append(x/y)
-    # Parallel Movement
-    if len(set(D)) == 1:
+    # Both Same Position Case
+    if all([x == y for x, y in zip(position1, position2)]):
+        return 0, 0
+
+    parallel = False
+    if vx1*vy2-vy1*vx2 == 0:  # Determinant test
+        parallel = True
+    # Parallel Movement Case
+    if parallel:
         ttc = []
         # Check for Overlapping Tracks
         for i, j in zip([cx1-cx2, cy1-cy2], [vx1-vx2, vy1-vy2]):  # Check if both can reach same point
@@ -63,7 +58,7 @@ def metric(position1, position2, velocity1, velocity2):
                 continue  # Linear dependence in 2D, first ttc value doesnt matter
             else:  # Time to travel dx or dy given vx and vy
                 ttc.append(abs(i/j))  # Time away from each other in x and y direction
-        # ttc should have repeated values or one 0 and other not.
+        # ttc should have repeated values or one 0 and other not 0.
         if len(set(ttc)) == 1:  # Overlapping (direction of movement allows one point to another)
             pet_xy1 = [0 if x == 0 or y == 0 else x/y for x, y in zip([cx1-cx2, cy1-cy2], velocity2)]
             pet_xy2 = [0 if x == 0 or y == 0 else x/y for x, y in zip([cx2-cx1, cy2-cy1], velocity1)]
@@ -71,6 +66,7 @@ def metric(position1, position2, velocity1, velocity2):
             return pet, ttc[0]
         else:  # One point can't get to another
             return None, None
+
     # Calculate time to intersection point for each car
     t2 = (vx1 * cy1 - vx1 * cy2 + vy1 * cx2 - vy1 * cx1) / (vx1 * vy2 - vx2* vy1)
     if vx1 != 0:  # If all four are zero both vehicles are still
